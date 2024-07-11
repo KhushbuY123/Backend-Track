fd95be60faedd2622d92ec97a81b3637a913d05b
Khushbu Yadav <khushbuyad123@gmail.com>
2024-07-11 11:43:18 +0530

    onstrcutor

--- Miscelleneous/Frontend/app.js
+++ Miscelleneous/Frontend/app.js
@@ -1,16 +1,25 @@
-const stud1={
-    name:"dama",
-    age:24,
-    marks:95,
-    getMarks:function(){
-        return this.marks;
-    }
-};
-const stud2={
-    name:"adam",
-    age:90,
-    marks:40,
-    getMarks:function(){
-        return this.marks;
-    }
+// ........Factory Function...................
+// function Person(name,age){
+//     const person={
+//     name=name,
+//     age=age,
+//     talk(){
+//         console.log(`Hi, I'm ${this.name}`)
+//     }
+//     }
+//     return person
+// }
+
+// let p11=Person("adma",24);
+// let p21=Person("adam",50);
+
+//.....Constructors - doesn't return anything & start with capital...........New Operator..................
+function Person(name,age){
+        this.name=name,
+        this.age=age,
+        }
+Person.prototype.talk=function(){
+    console.log(`Hi , I'm ${this.name}`);
 }
+let p1=new Person("adma",24);
+let p2=new Person("adam",50);
