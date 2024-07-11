e0af1789e10feb7e322032b1d57aa80e9570c79b
Khushbu Yadav <khushbuyad123@gmail.com>
2024-07-11 12:21:21 +0530

    Inheritence

--- Miscelleneous/Frontend/app.js
+++ Miscelleneous/Frontend/app.js
@@ -9,17 +9,48 @@
 //     }
 //     return person
 // }
-
 // let p11=Person("adma",24);
 // let p21=Person("adam",50);
 
+
+
+
 //.....Constructors - doesn't return anything & start with capital...........New Operator..................
-function Person(name,age){
-        this.name=name,
-        this.age=age,
-        }
-Person.prototype.talk=function(){
-    console.log(`Hi , I'm ${this.name}`);
+// function Person(name,age){
+//         this.name=name,
+//         this.age=age
+//         }
+// Person.prototype.talk=function(){
+//     console.log(`Hi , I'm ${this.name}`);
+// }
+// let p1=new Person("adma",24);
+// console.log(p1)
+// let p2=new Person("adam",50);
+// console.log(p2)
+
+
+
+
+//...................Classes................
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    talk(){
+        console.log(`Hi , my name is ${this.name}`);
+    }
 }
-let p1=new Person("adma",24);
-let p2=new Person("adam",50);
+//.............Inheritence...........................
+class Student extends Person{
+    constructor(name,age,marks){
+        super(name,age);
+        this.marks=marks;
+    }
+    greet(){
+        return "Hello"
+    }
+}
+let s1=new Student("adman",23,56)
+console.log(s1)
+
