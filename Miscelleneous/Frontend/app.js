560ae4f03dfdb0cb1bb887f13b6ba994555acac6
Khushbu Yadav <khushbuyad123@gmail.com>
2024-07-11 10:38:22 +0530

    OOPs

---
+++ Miscelleneous/Frontend/app.js
@@ -0,0 +1,16 @@
+const stud1={
+    name:"dama",
+    age:24,
+    marks:95,
+    getMarks:function(){
+        return this.marks;
+    }
+};
+const stud2={
+    name:"adam",
+    age:90,
+    marks:40,
+    getMarks:function(){
+        return this.marks;
+    }
+}
